f514601597440105540569c3c7636ff09a323062
superstar54 <xingwang1991@gmail.com>
2023-11-27 13:50:58 +0100

    add protocol in doc

--- tests/test_protocol.py
+++ tests/test_protocol.py
@@ -4,7 +4,7 @@ import numpy as np
 aiida.load_profile()
 
 
-def test_pw_relax_workchain(structure_si):
+def test_pw_relax_protocol(structure_si):
     """Run simple calcfunction."""
     from aiida_worktree import build_node, WorkTree
     from aiida import orm
@@ -21,7 +21,29 @@ def test_pw_relax_workchain(structure_si):
     )
     wt.submit(wait=True, timeout=200)
     assert wt.state == "FINISHED"
-    # print(wt.nodes["pw_relax1"].node.outputs.output_parameters["energy"])
-    assert np.isclose(
-        wt.nodes["pw_relax1"].node.outputs.output_parameters["energy"], -308.46262827125
+    assert "base_final_scf" in wt.nodes["pw_relax1"].node.inputs
+    energy = wt.nodes["pw_relax1"].node.outputs.output_parameters["energy"]
+    assert np.isclose(energy, -308.46, atol=0.01)
+
+
+def test_pw_relax_protocol_pop(structure_si):
+    """Run simple calcfunction."""
+    from aiida_worktree import build_node, WorkTree
+    from aiida import orm
+
+    # register node
+    pw_relax_node = build_node(
+        {"path": "aiida_quantumespresso.workflows.pw.relax.PwRelaxWorkChain"}
     )
+    code = orm.load_code("pw-7.2@localhost")
+    wt = WorkTree("test_pw_relax")
+    pw_relax1 = wt.nodes.new(pw_relax_node, name="pw_relax1")
+    pw_relax1.set_from_protocol(
+        code, structure_si, protocol="fast", pseudo_family="SSSP/1.2/PBEsol/efficiency"
+    )
+    # do not run "base_final_scf"
+    pw_relax1.inputs["base_final_scf"].value = None
+    wt.submit(wait=True, timeout=200)
+    assert "base_final_scf" not in wt.nodes["pw_relax1"].node.inputs
+    energy = wt.nodes["pw_relax1"].node.outputs.output_parameters["energy"]
+    assert np.isclose(energy, -308.46, atol=0.01)
