a1980674eda862394ddc2d8e4f7ed0421d15b4ee
superstar54 <xingwang1991@gmail.com>
2024-04-19 17:18:46 +0200

    add test data type

---
+++ tests/test_data_type.py
@@ -0,0 +1,15 @@
+from aiida_workgraph import WorkGraph
+import aiida
+
+aiida.load_profile()
+
+
+def test_numpy_array(decorated_normal_add):
+    """Test data type with numpy array."""
+    import numpy as np
+
+    x = np.array([1, 2, 3])
+    y = np.array([4, 5, 6])
+    wg = WorkGraph()
+    wg.nodes.new(decorated_normal_add, name="add1", x=x, y=y)
+    wg.submit()
