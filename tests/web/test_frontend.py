f225e54473afa9429de33291aa90323e201d95b7
superstar54 <xingwang1991@gmail.com>
2024-07-10 09:23:34 +0200

    add visual comparison test

--- tests/web/test_frontend.py
+++ tests/web/test_frontend.py
@@ -28,7 +28,6 @@ def test_workgraph(page, wg_calcfunction):
     assert page.locator(".search-input").is_visible()
 
     # Verify the presence of the table header columns
-    # Verify the presence of the table header columns
     assert page.locator("th:has-text('PK')").is_visible()
     assert page.locator("th:has-text('Created')").is_visible()
     assert page.locator("th:has-text('Process Label')").is_visible()
@@ -43,7 +42,7 @@ def test_workgraph(page, wg_calcfunction):
     assert page.locator("tr").count() >= 2  # Including header row
 
 
-def test_workgraph_item(page, wg_calcfunction):
+def test_workgraph_item(page, wg_calcfunction, assert_snapshot):
 
     wg = wg_calcfunction
     wg.submit(wait=True)
@@ -53,13 +52,8 @@ def test_workgraph_item(page, wg_calcfunction):
     page.get_by_text("sumdiff3").is_visible()
 
     # Simulate user interaction (e.g., clicking a button)
-    # Replace the selector with the actual selector of the button you want to click
-    # You should identify the button that triggers an action in your component
     page.get_by_role("button", name="Arrange").click()
     page.wait_for_timeout(8000)
-    # Capture a screenshot
-    screenshot = page.screenshot()
 
-    # Save the screenshot to a file
-    with open("screenshot.png", "wb") as f:
-        f.write(screenshot)
+    # compare the screenshot
+    assert_snapshot(page.screenshot(), "screenshot.png")
