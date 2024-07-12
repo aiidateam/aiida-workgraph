eaa660e366bc6395c357b0cde71176f33a03c0f5
superstar54 <xingwang1991@gmail.com>
2024-07-12 14:51:05 +0200

    add title and description

--- aiida_workgraph/web/frontend/src/components/WorkGraphDuration.js
+++ aiida_workgraph/web/frontend/src/components/WorkGraphDuration.js
@@ -27,8 +27,8 @@ const NodeDurationGraph = ({ id }) => {
 
     useEffect(() => {
         fetchData();
-        const interval = setInterval(fetchData, 5000); // Adjust interval as needed
-        return () => clearInterval(interval); // Clear interval on cleanup
+        const interval = setInterval(fetchData, 5000);
+        return () => clearInterval(interval);
     }, [id]);
 
     useEffect(() => {
@@ -55,15 +55,14 @@ const NodeDurationGraph = ({ id }) => {
                 const validStartTimes = newItems.map(item => item.start_time).filter(time => time);
                 const validEndTimes = newItems.map(item => item.end_time).filter(time => time);
                 if (validStartTimes.length && validEndTimes.length) {
-                    setTimeStart(moment.min(validStartTimes).valueOf()); // Convert moment to milliseconds
-                    setTimeEnd(moment.max(validEndTimes).valueOf()); // Convert moment to milliseconds
+                    setTimeStart(moment.min(validStartTimes).valueOf());
+                    setTimeEnd(moment.max(validEndTimes).valueOf());
                 }
                 setInitialLoad(false);
             }
         }
     }, [processesInfo]);
 
-    // Define minimum and maximum zoom (in milliseconds)
     const minZoom = 10000; // 10 seconds in milliseconds
     const maxZoom = 365.25 * 24 * 60 * 60 * 1000; // 1 year in milliseconds
 
@@ -72,23 +71,34 @@ const NodeDurationGraph = ({ id }) => {
     }
 
     return (
-        <Timeline
-            groups={groups}
-            items={items}
-            visibleTimeStart={timeStart}
-            visibleTimeEnd={timeEnd}
-            onTimeChange={(visibleTimeStart, visibleTimeEnd, updateScrollCanvas) => {
-                setTimeStart(visibleTimeStart);
-                setTimeEnd(visibleTimeEnd);
-                updateScrollCanvas(visibleTimeStart, visibleTimeEnd);
-            }}
-            lineHeight={50}
-            minZoom={minZoom}
-            maxZoom={maxZoom}
-            canMove={false}
-            canChangeGroup={false}
-            canResize={'both'}
-        />
+            <div style={{ padding: '10px', margin: '20px', border: '1px solid #ccc',  }}>
+            <h1 style={{ textAlign: 'center', color: '#2a3f5f' }}>Node Process Timeline</h1>
+            <div style={{textAlign: 'left', fontSize: '16px', color: '#555' }}>
+                <p>
+                The timeline uses bars to represent the active periods of process nodes, marked from their creation (ctime) to their last modification (mtime). It's important to note that these timestamps do not necessarily correlate with the actual running time since processes might be queued or paused.
+                </p>
+                <p>
+                    Data nodes are shown as rows without bars.
+                </p>
+                </div>
+            <Timeline
+                groups={groups}
+                items={items}
+                visibleTimeStart={timeStart}
+                visibleTimeEnd={timeEnd}
+                onTimeChange={(visibleTimeStart, visibleTimeEnd, updateScrollCanvas) => {
+                    setTimeStart(visibleTimeStart);
+                    setTimeEnd(visibleTimeEnd);
+                    updateScrollCanvas(visibleTimeStart, visibleTimeEnd);
+                }}
+                lineHeight={50}
+                minZoom={minZoom}
+                maxZoom={maxZoom}
+                canMove={false}
+                canChangeGroup={false}
+                canResize={'both'}
+            />
+        </div>
     );
 };
 
