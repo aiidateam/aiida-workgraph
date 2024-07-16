616a096e878e40ab98a90adb5d122e5b5a5d87c1
superstar54 <xingwang1991@gmail.com>
2024-07-16 23:34:09 +0200

    serialize task.process in `to_dict`

--- aiida_workgraph/task.py
+++ aiida_workgraph/task.py
@@ -57,12 +57,14 @@ class Task(GraphNode):
         self.action = ""
 
     def to_dict(self) -> Dict[str, Any]:
+        from aiida.orm.utils.serialize import serialize
+
         tdata = super().to_dict()
         tdata["context_mapping"] = self.context_mapping
         tdata["wait"] = [
             task if isinstance(task, str) else task.name for task in self.wait
         ]
-        tdata["process"] = self.process.uuid if self.process else None
+        tdata["process"] = serialize(self.process) if self.process else serialize(None)
         tdata["metadata"]["pk"] = self.process.pk if self.process else None
         tdata["metadata"]["is_aiida_component"] = self.is_aiida_component
 
