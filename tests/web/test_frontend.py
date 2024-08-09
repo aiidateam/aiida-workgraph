3574c38375fa1c890e4d6a3f6c0397708f515c86
Alexander Goscinski <alexander.goscinski@posteo.de>
2024-08-09 14:37:18 +0200

    Start web server using uvicorn for frontend tests (#202)
    * The frontend tests were not working before because no web server was there
      to connect to. We now start a web server in a separate processes so it does
      not conflict with the asyncio event loop of AiiDA.
    
    * Add settings for backend server that can be set through environment
      variables. This allows to specifiy the aiida profile that should be loaded on
      initialization. Implements part of feature request in issue #159.
    
    * Split up frontend and backend fixtures and utilities into their corresponding
      files.
    
    * Mark frontend and backend tests with pytest
    
    * Add option to run frontent tests headed using environemnt variable
    
    * Add CONTRIBUTING.md that explains running of tests, specifically the frontend
      and backend tests.

--- tests/web/test_frontend.py
+++ tests/web/test_frontend.py
@@ -1,7 +1,132 @@
 import pytest
 
+from playwright.sync_api import sync_playwright
 
-def test_homepage(page):
+import uvicorn
+
+from multiprocessing import Process, Value
+
+import contextlib
+import threading
+import time
+import os
+
+################################
+# Utilities for frontend tests #
+################################
+
+
+class UvicornTestServer(uvicorn.Server):
+    """
+    Suggested way to start a server in a background by developers
+    https://github.com/encode/uvicorn/discussions/1103#discussioncomment-941726
+    """
+
+    def install_signal_handlers(self):
+        pass
+
+    @contextlib.contextmanager
+    def run_in_thread(self):
+        thread = threading.Thread(target=self.run)
+        thread.start()
+        try:
+            while not self.started:
+                time.sleep(1e-3)
+            yield
+        finally:
+            self.should_exit = True
+            thread.join()
+
+
+def run_uvicorn_web_server(
+    web_server_started, stop_web_server, **uvicorn_configuration
+):
+    config = uvicorn.Config(**uvicorn_configuration)
+    uvicorn_web_server = UvicornTestServer(config=config)
+    with uvicorn_web_server.run_in_thread():
+        with web_server_started.get_lock():
+            web_server_started.value = 1
+        while not stop_web_server.value:
+            time.sleep(1e-3)
+
+
+###############################
+# Fixtuers for frontend tests #
+###############################
+
+
+@pytest.fixture(scope="module")
+def uvicorn_configuration():
+    return {
+        "app": "aiida_workgraph.web.backend.app.api:app",
+        "host": "0.0.0.0",
+        "port": 8000,
+        "log_level": "info",
+        "workers": 2,
+    }
+
+
+@pytest.fixture(scope="module")
+def web_server(set_backend_server_settings, uvicorn_configuration):
+    from ctypes import c_int8
+
+    web_server_started = Value(c_int8, 0)
+    stop_web_server = Value(c_int8, 0)
+    web_server_proc = Process(
+        target=run_uvicorn_web_server,
+        args=(web_server_started, stop_web_server),
+        kwargs=uvicorn_configuration,
+    )
+
+    web_server_proc.start()
+
+    while not web_server_started.value:
+        time.sleep(1e-3)
+
+    yield web_server_proc
+
+    with stop_web_server.get_lock():
+        stop_web_server.value = 1
+
+    web_server_proc.join()
+    web_server_proc.close()
+
+
+# Define a fixture for the browser
+@pytest.fixture(scope="module")
+def browser():
+    pytest_playwright_headless = os.environ.get("PYTEST_PLAYWRIGHT_HEADLESS", "yes")
+    if pytest_playwright_headless == "yes":
+        headless = True
+    elif pytest_playwright_headless == "no":
+        headless = False
+    else:
+        raise ValueError(
+            f"Found environment variable PYTEST_PLAYWRIGHT_HEADLESS={pytest_playwright_headless}, "
+            'please use "yes" or "no"'
+        )
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=headless)
+        yield browser
+        browser.close()
+
+
+# Define a fixture for the page
+@pytest.fixture(scope="module")
+def page(browser):
+    with browser.new_page() as page:
+        yield page
+        page.close()
+
+
+##################
+# Frontend tests #
+##################
+
+
+@pytest.mark.frontend
+def test_homepage(web_server, page):
     page.goto("http://localhost:8000")
 
     assert page.title() == "AiiDA-WorkGraph App"
@@ -15,12 +140,14 @@ def test_homepage(page):
         pytest.fail("Element 'a[href='/wortre']' not found on the page")
 
 
-@pytest.mark.skip(
-    reason="The test does not work because rest api does not the profile fixture."
-)
-def test_workgraph(page, wg_calcfunction):
+@pytest.mark.frontend
+def test_workgraph(web_server, page, aiida_profile, wg_calcfunction):
     wg_calcfunction.run()
-    page.goto("http://localhost:8000/workgraph")
+
+    page.goto("http://localhost:8000")
+    # Since the routing is done by react-router-dom we cannot access it with a call like this
+    # page.goto("http://localhost:8000/workgraph" but have to navigate to it
+    page.click('a[href="/workgraph"]')
 
     # Check for the existence of a specific element on the page
 
@@ -43,11 +170,11 @@ def test_workgraph(page, wg_calcfunction):
 
     # Verify the presence of at least one row in the table
     page.wait_for_timeout(8000)
-    assert page.locator("tr").count() >= 2  # Including header row
+    assert page.locator("tr").count() >= 1  # Including header row
 
 
+@pytest.mark.frontend
 def test_workgraph_item(page, wg_calcfunction):
-
     wg = wg_calcfunction
     wg.run()
     page.goto("http://localhost:8000/workgraph/{}".format(wg.pk))
