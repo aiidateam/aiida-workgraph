f28be4fe1408a3395470954c124a1cd8f2705995
superstar54 <xingwang1991@gmail.com>
2024-08-26 13:01:54 +0200

    Add test for empty if

--- tests/test_if.py
+++ tests/test_if.py
@@ -31,3 +31,12 @@ def test_if_task(decorated_add, decorated_multiply, decorated_compare):
     add3 = wg.add_task(decorated_add, name="add3", x=select1.outputs["result"], y=1)
     wg.run()
     assert add3.outputs["result"].value == 5
+
+
+def test_empty_if_task():
+    """Test the If task with no children."""
+
+    wg = WorkGraph("test_empty_if")
+    wg.add_task("If", name="if_true")
+    wg.run()
+    assert wg.state == "FINISHED"
