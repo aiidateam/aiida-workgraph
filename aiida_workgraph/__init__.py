9d7af52b1498d021951c008bdbc73be7f368ce67
superstar54 <xingwang1991@gmail.com>
2024-10-30 23:20:27 +0100

    Release 0.4.3

--- aiida_workgraph/__init__.py
+++ aiida_workgraph/__init__.py
@@ -9,6 +9,6 @@ from .workgraph import WorkGraph
 from .task import Task
 from .decorator import task, build_task
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 
 __all__ = ["WorkGraph", "Task", "task", "build_task"]
