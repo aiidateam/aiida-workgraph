1353492d892517d110ff5f60ea164e072673c974
Xing Wang <xingwang1991@gmail.com>
2025-07-12 15:01:30 +0200

    Refactor `@task.graph_builder ` to `@task.graph` (#554)
    Because the `graph_builder` is difficult to use in the context manager approach, for example: How to call the `graph_buider` as a task, i.e., as a black box. This is because the `graph_builder` returns a WorkGraph, while in the context manager approach, one calls every component as a task and expects it to return the outputs. Even though `graph_builder` is successfully used in the node-graph programming approach, it is created before the context manager approach. Thus, in this PR, we refactor it to make it work more consistently inside the context manager approach, while still usable in the node-graph programming approach.
    
    The main changes are:
    - the `graph task` returns the WorkGraph outputs, instead of the WorkGraph itself
    - provide a `get_graph` method

--- tests/test_workgraph.py
+++ tests/test_workgraph.py
@@ -164,15 +164,15 @@ def test_extend_workgraph(decorated_add_multiply_group):
 
     wg = WorkGraph("test_graph_build")
     add1 = wg.add_task("workgraph.test_add", "add1", x=2, y=3)
-    add_multiply_wg = decorated_add_multiply_group(x=0, y=4, z=5)
+    add_multiply_wg = decorated_add_multiply_group.get_graph(x=0, y=4, z=5)
     # test wait
-    add_multiply_wg.tasks.multiply1.waiting_on.add("add1")
+    add_multiply_wg.tasks.multiply.waiting_on.add("add")
     # extend workgraph
     wg.extend(add_multiply_wg, prefix="group_")
-    assert "group_add1" in [task.name for task in wg.tasks.group_multiply1.waiting_on]
-    wg.add_link(add1.outputs[0], wg.tasks.group_add1.inputs.x)
+    assert "group_add" in [task.name for task in wg.tasks.group_multiply.waiting_on]
+    wg.add_link(add1.outputs[0], wg.tasks.group_add.inputs.x)
     wg.run()
-    assert wg.tasks.group_multiply1.outputs.result == 45
+    assert wg.tasks.group_multiply.outputs.result == 45
 
 
 def test_workgraph_outputs(decorated_add):
