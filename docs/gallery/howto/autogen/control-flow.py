058dba8fd94f1c3c7fec28ebbae3543553aa3f57
Julian Geiger <julian.geiger@gmx.net>
2025-07-16 11:38:40 +0200

    wip

--- docs/gallery/howto/autogen/control-flow.py
+++ docs/gallery/howto/autogen/control-flow.py
@@ -7,7 +7,7 @@ Control flow in WorkGraph
 # Introduction
 # ============
 # In this how-to we show you how you can implement the ``while`` loop and ``if`` conditional flow control elements in ``WorkGraph``.
-# You can implement both using the context manager approach, as well as using the ``graph_builder`` for the ``if`` conditional (``while`` is currently not supported by the ``graph_builder``).
+# You can implement both using the context manager approach, as well as using the ``@task.graph`` decorator for the ``if`` conditional (``while`` is currently not supported).
 # So let's dive right into it!
 
 # %%
@@ -21,20 +21,34 @@ from aiida import load_profile
 
 load_profile()
 
-# %%
-# While
-# =====
 
-#%% 
+# %%
+# If
+# ==
+
+# %%
 # Workflow description
 # --------------------
-# Suppose we want to implement a workflow with the following tasks:
+#
+# Now, let's see how we can convert the following arithmetic workflow containing an ``if`` conditional into a WorkGraph:
+#
+# .. code-block:: python
+#
+#    # First addition
+#    result = 1 + 1
+#
+#    # Conditionally execute addition or multiplication
+#    if result < 0:
+#        result = result + 2
+#    else:
+#        result = result * 3
+#
+#    # Last addition
+#    result = result + 1
 
-
-@task
-def compare(x, y):
-    return x < y
-
+# %%
+# We now define the relevant arithmetic operations as WorkGraph tasks.
+# They will present the processes executed in the workflow and their provenance will be tracked.
 
 @task
 def add(x, y):
@@ -45,116 +59,38 @@ def add(x, y):
 def multiply(x, y):
     return x * y
 
-
-# %%
-# And the following workflow logic expressed in Python:
-#
-# .. code-block:: python
-#
-#    n = add(1, 1)
-#    while compare(n, 8):
-#        n = add(n, 1)
-#        n = multiply(n, 2)
-#    result = add(n, 1)
-
-# %%
-# Context manager
-# ---------------
-# Unlike regular tasks, the *while zone* lacks data input and output sockets.
-# Tasks outside the zone can directly link to those inside, facilitating a workflow integration.
-# We have the option to specify the maximum number of iterations to prevent an infinite loop.
-
-with WorkGraph("while_context_manager") as wg:
-    # set a context variable before running.
-    outputs1 = add(x=1, y=1)
-    wg.ctx.n = outputs1.result
-    # We need to use context or static variables here
-    outputs2 = compare(x=wg.ctx.n, y=8)
-    # Because context variables do not follow the dataflow programming paradigm,
-    # we have to state the dependency of the context variable explicitely.
-    # The syntax below reads "`should_run` associated task waits for its executaion till `outputs1` has been successfully set its value"
-    outputs2 << outputs1
-    with While(outputs2.result, max_iterations=10):
-        outputs3 = add(x=wg.ctx.n, y=1)
-        outputs4 = multiply(x=outputs3.result, y=2)
-        # We need to update the context variable since it is accessed in should_run task
-        wg.ctx.n = outputs4.result
-    wg.outputs.result = add(x=outputs4.result, y=1).result
-
-wg.run()
-print("State of WorkGraph:   {}".format(wg.state))
-print("Result            :   {}".format(wg.outputs.result.value))
-# 2 -> While(3, 6 -> 7, 14) -> 15
-assert wg.outputs.result.value == 15
-
-# %%
-# Workflow view
-# ~~~~~~~~~~~~~
-# In the graphical workflow view the ``While`` context manager is depicted as a *zone*, containing all its child tasks.
-# This zone simplifies the visualization of the loop structure as it separates the logic executed within the loop from the one outside.
-# Notice that the cyclic links around the context variable ``n`` since it is reused each iteration.
-
-wg.to_html()
-
-# %%
-# Provenance graph
-# ~~~~~~~~~~~~~~~~
-
-generate_node_graph(wg.pk)
-
-# %%
-# If
-# ==
-
-# %%
-# Workflow description
-# --------------------
-#
-# Now, using the same ``add`` and ``multiply`` tasks as defined for the ``while`` workflow, suppose you have the following workflow logic
-
-# %%
-# And the following workflow logic expressed in Python:
-#
-# .. code-block:: python
-#
-#    # First addition
-#    result = add(1, 1)
-#
-#    # Conditionally execute addition or multiplication
-#    if result < 0:
-#        result = add(result, 2)
-#    else:
-#        result = multiply(result, 3)
-#
-#    # Last addition
-#    result = add(result, 1)
-
 # %%
 # Context manager
 # ---------------
 #
-# WorkGraph provides the ``If`` context manager that allows you to define conditional logic in your workflow.
+# For this, WorkGraph also provides an ``If`` context manager, which allows you to define conditional logic in your workflow.
 # The ``If`` block encapsulates all its child tasks, which are executed based on the defined conditions.
 
-with WorkGraph("if_context_manager") as wg:
-    result = add(x=1, y=1).result
-    with If(result < 0):
-        r1 = add(x=result, y=2).result
-        wg.ctx.result = r1
-    with If(result >= 0):
-        r2 = multiply(x=result, y=3).result
-        wg.ctx.result = r2
+with WorkGraph("if_task") as wg:
+    outputs1 = add(x=1, y=1)
+    with If(outputs1.result < 0):
+        outputs2 = add(x=outputs1.result, y=2)
+        wg.ctx.result = outputs2.result
+    with If(outputs1.result >= 0):
+        outputs3 = multiply(x=outputs1.result, y=3)
+        wg.ctx.result = outputs3.result
 
-    # -----------------------------------------
-    result = add(x=wg.ctx.result, y=1).result
-    r1 >> result
-    r2 >> result
-    wg.outputs.result = result
+    outputs4 = add(x=wg.ctx.result, y=1)
+    # Again, we have to use the ``>>`` syntax here to explicitly tell WG to wait with the execution of the last task,
+    # until the previous two tasks have finished.
+    outputs2 >> outputs4
+    outputs3 >> outputs4
+    # In principle, as we are dealing with python functions that are run in a blocking manner here, the example would
+    # also work without ``>>``. However, if the tasks would be submitted to the deamon in a non-blocking fashion, the
+    # explicit waiting enforced by ``>>`` is required.
+
+    # Finally, we set the result of the last task as the global workflow output result
+    wg.outputs.result = outputs4.result
 
 wg.run()
-print("State of WorkGraph:   {}".format(wg.state))
-print("Result            :   {}".format(wg.outputs.result.value))
-# import ipdb; ipdb.set_trace()
+print(f"State of WorkGraph: {wg.state}")
+print(f"Result            : {wg.outputs.result.value}")
+
 assert wg.outputs.result.value == 7
 
 # %%
@@ -168,127 +104,137 @@ wg.to_html()
 # %%
 # Provenance graph
 # ~~~~~~~~~~~~~~~~
-
-generate_node_graph(wg.pk)
-
-# %%
-# Submit the WorkGraph
-# ~~~~~~~~~~~~~~~~~~~~
-
-wg.run()
-print(f"State of WorkGraph: {wg.state}")
-print(f"Result:             {result.value}")
-
-# %%
 # Finally, after the WG has finished, we generate the node (provenance) graph from the AiiDA process, where we can see
 # that the result of the ``op_lt`` (larger than) comparison is ``False``, while for the ``og_ge`` (greater or equal)
 # comparison it is ``True``, meaning that the branch with the intermediate multiplication was executed.
 
 generate_node_graph(wg.pk)
 
-
 # %%
-# Graph builder
-# -------------
+# Graph decorator
+# ---------------
 #
-# The ``graph_builder`` decorator is used for creating a dynamic ``WorkGraph`` during runtime based on input values (see `this
-# section <https://aiida-workgraph.readthedocs.io/en/latest/howto/autogen/graph_builder.html>`_).
+# The ``graph`` decorator is used for creating a dynamic ``WorkGraph`` during runtime based on input values (see `this
+# section <https://aiida-workgraph.readthedocs.io/en/latest/howto/autogen/graph.html>`_).
 #
 # This method differs significantly from the ``If`` context manager:
 #
-# - **Visibility**: In the GUI, only the ``graph_builder`` task is visible before execution, while for the ``If``,
+# - **Visibility**: In the GUI, only the ``graph`` task is visible before execution, while for the ``If``,
 #   both branches were shown
-# - **Dynamic Generation**: Upon running, it generates the WorkGraph dynmically, allowing for complex conditional logic and flow adjustments based on runtime data.
+# - **Dynamic Generation**: Upon running, it generates the WorkGraph dynamically, allowing for complex conditional logic and flow adjustments based on runtime data.
 
 # Create a WorkGraph which is dynamically generated based on the input
 # then we output the result as a graph-level output
-@task.graph_builder(outputs=["result"])
-def add_multiply_if(x, y):
-    with WorkGraph("add_multiply_graph_builder") as wg:
-        if x.value < 0:
-            add1 = add(x=x, y=y)
-            # export the result of add1 to the graph-level outputs
-            wg.outputs.result = add1.outputs.result
-        else:
-            multiply1 = multiply(x=x, y=y)
-            # export the result of multiply1 to the graph-level outputs
-            wg.outputs.result = multiply1.outputs.result
-    return wg
+@task.graph()
+def add_multiply_if(x, y, z):
+    if x.value < 0:
+        return add(x=x, y=y).result
+    else:
+        return multiply(x=x, y=z).result
 
-# 
-with WorkGraph("if_graph_builer") as wg:
+with WorkGraph("if_graph") as wg:
     add1 = wg.add_task(add, name="add1", x=1, y=1)
     add_multiply_if1 = wg.add_task(
-        add_multiply_if, name="add_multiply_if1", x=add1.outputs.result, y=3
+        add_multiply_if, name="add_multiply_if1", x=add1.outputs.result, y=2, z=3
     )
     add1 = wg.add_task(add, name="add2", x=add_multiply_if1.outputs.result, y=1)
-
-# We export the workgraph to an html file so that it can be visualized in a browser
-wg.to_html()
-
-# Comment out the following line to visualize the workgraph in jupyter-notebook
-# wg
-
-# %%
-# In the GUI, we only see the ``add_multiply_if1`` task. When this task run, it will generate a ``WorkGraph`` based on the input value. This is different from the ``If`` task, in which we see all tasks before the WorkGraph run.
-
-
-# %%
-# Submit the WorkGraph and check the results
-# ------------------------------------------
 
 wg.run()
 print(f"State of WorkGraph: {wg.state}")
 print(f"Result            : {wg.tasks.add2.outputs.result.value}")
 
 # %%
+# Workflow view
+# ~~~~~~~~~~~~~
+#
+# TODO: Continue here
+# In the GUI, we only see the ``add_multiply_if1`` task. When this task run, it will generate a ``WorkGraph`` based on the input value. This is different from the ``If`` task, in which we see all tasks before the WorkGraph run.
+
+wg.to_html()
+
+# %%
+# Provenance graph
+# ~~~~~~~~~~~~~~~~
 # Generate node graph from the AiiDA process,and we can see that the ``multiply`` task is executed.
 
 generate_node_graph(wg.pk)
 
 # %%
-# Using the If Task
-# =================
-#
-# Internally, the ``If`` context manager is implemented using the ``If`` ``Task`` from the WorkGraph library.
-# In the WorkGraph user interface, the ``If`` ``Task`` is visually represented as an "If Zone".
-# This zone encapsulates all its child tasks, which are executed based on the defined conditions.
-#
-# - **conditions**: The If Zone includes a ``conditions`` socket, which determines when the tasks inside the zone should be executed.
-# - **invert_condition**: If this input is True, it will invert the conditions.
-# - **Task Linking**: Tasks located outside the If Zone can be directly linked to tasks within the zone, allowing for dynamic workflow adjustments based on conditional outcomes.
-#
-# Here is an example of how to add an ``If`` ``Task`` to a WorkGraph:
-#
+# While
+# =====
+
+# %%
+# Workflow description
+# --------------------
+# Suppose we want to implement a workflow with the following logic:
+
 # .. code-block:: python
 #
-#     if_task = wg.add_task(
-#        "workgraph.if_zone",
-#        name="if_false",
-#        conditions=condition1.outputs["result"],
-#        invert_condition=True
-#     )
+#    n = 1 + 1
+#    while n < 8:
+#        n = n + 1
+#        n = n * 2
+#        print(n)
+#    result = n + 1
 
 # %%
-# Adding tasks to the If Zone
-# ---------------------------
-# We can add tasks to the ``If`` zone using the ``children`` attribute.
-#
-# .. code-block:: python
-#
-#     # add task1 and task2 to the if zone
-#     if_task.children.add(["task1", "task2"])
+# We have to define our comparison operation as a task, as well
+# PRCOMMENT: Why??
 
+@task
+def compare_lt(x, y):
+    return x < y
 
 # %%
-# Summary
-# =======
-#
-# The ``If`` provides a visual and structured approach to managing conditional tasks within a defined zone. In contrast,
-# the ``graph_builder`` decorator offers flexibility by dynamically generating the workflow based on runtime inputs,
-# suitable for complex and adaptive process flows.
-
-# %%
-# Further reading
+# Context manager
 # ---------------
-# Similarly, other the control logic can implemented, see `Use if condition  <../if.ipynb>`_ for if logic and `How to run tasks in parallel <parallel.py>`_ for map logic.
+# Now, we can construct the WorkGraph using the ``While`` context manager,
+# which effectively creates a ``while zone`` (as seen in the workflow view below).
+# Unlike regular tasks, the *while zone* lacks data input and output sockets.
+# Tasks outside the zone can directly link to those inside, facilitating workflow integration.
+# We have the option to specify the maximum number of iterations to prevent an infinite loop.
+
+with WorkGraph("while_context_manager") as wg:
+    initial_n = add(x=1, y=1).result
+    wg.ctx.n = initial_n
+    should_run = compare_lt(x=wg.ctx.n, y=8).result
+    should_run << initial_n
+    with While(should_run, max_iterations=10):
+        n = add(x=wg.ctx.n, y=1).result
+        n = multiply(x=n, y=2).result
+        wg.ctx.n = n
+
+    wg.outputs.result = add(x=n, y=1).result
+
+wg.run()
+print(f"State of WorkGraph: {wg.state}")
+print(f"Result            : {wg.outputs.result.value}")
+# 2 -> While(3, 6 -> 7, 14) -> 15
+assert wg.outputs.result.value == 15
+
+
+# %% Notes on the example before
+
+# set a context variable before running.
+# We need to use context or static variables here
+# Because context variables do not follow the dataflow programming paradigm,
+# we have to state the dependency of the context variable explicitly.
+# The syntax below reads "``should_run`` associated task waits for its
+# execution till ``outputs1`` has been successfully set its value"
+
+# We need to update the context variable since it is accessed in should_run task
+
+# %%
+# Workflow view
+# ~~~~~~~~~~~~~
+# In the graphical workflow view the ``While`` context manager is depicted as a *while zone*, containing all its child tasks.
+# This zone simplifies the visualization of the loop structure as it separates the logic executed within the loop from the one outside.
+# Notice that the cyclic links around the context variable ``n`` since it is reused each iteration.
+
+wg.to_html()
+
+# %%
+# Provenance graph
+# ~~~~~~~~~~~~~~~~
+
+generate_node_graph(wg.pk)
