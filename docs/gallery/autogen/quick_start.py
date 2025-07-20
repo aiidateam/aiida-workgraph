e5091e795e4285a4adb39415981396b3037b7166
Edan Bainglass <edan.bainglass@gmail.com>
2025-07-20 19:37:19 +0200

    Update quick start docs

--- docs/gallery/autogen/quick_start.py
+++ docs/gallery/autogen/quick_start.py
@@ -4,7 +4,6 @@ Quick Start
 
 """
 
-
 # %%
 # Introduction
 # ------------
@@ -15,74 +14,84 @@ Quick Start
 # - how to define and use
 #   - tasks to encapsulate a process
 #   - sub-workflows as tasks to encapsulate several sub-processes
-# - an advanced example highlighting briefly the various features of ``WorkGraph``
-# - a brief introduction to the web interface
+# - an advanced example demonstrating control flow in ``WorkGraph``
 
 # %%
 # Installation
 # ------------
 #
 # Let's first install ``aiida-workgraph``
-
-# ::
-#    pip install aiida-workgraph
 #
-# .. note::
+# .. code:: console
 #
-#    This will also install ``aiida-core`` and its dependencies.
+#    $ pip install aiida-workgraph
 #
-# .. note::
-#    If this is your first time using AiiDA, you will need to configure a profile.
-#    You can do this by running the following command in your terminal:
-
-#    ::
-#       verdi presto
+# This will also install ``aiida-core`` and its dependencies.
 
 # %%
-# Load an AiiDA profile
-# ---------------------
+# Setup
+# -----
+#
+# Logging
+# ~~~~~~~
+#
+# For brevity, we disable process reporting throughout the documentation.
+# We temporarily restore it here for demonstration purposes only.
+# **You can safely skip this step.**
+
+from aiida_workgraph.utils.logging import set_aiida_loglevel
+
+set_aiida_loglevel("REPORT")
+
+# %%
+# Profile
+# ~~~~~~~
 #
 # To interact with the AiiDA database, you need to load your AiiDA profile.
-# You have two options:
+# If you haven't configured one yet, you can do so by running the following command:
 #
-# 1. AiiDA terminal - You can run the following command to enter an AiiDA-configured ``IPython`` shell
+# .. code:: console
 #
-#    ::
-#       verdi shell
+#    $ verdi presto
 #
-#    .. note::
-#
-#       This will start an IPython shell with your AiiDA profile loaded.
-#       This environment is configured to work with AiiDA seamlessly, including such features as tab completion.
-#
-# 2. Alternatively, if working in a Jupyter notebook, you can load your profile with the following code:
+# To load your profile, add the following code to your script or Jupyter notebook:
 
 from aiida import load_profile
 
 load_profile()
 
 # %%
-# .. note::
+# .. tip::
+#
+#    AiiDA provides a pre-configured shell that automatically loads your profile.
+#    If you prefer a shell environment over a script or notebook, you can start it with:
+#
+#    .. code:: console
+#
+#       $ verdi shell
+
+# %%
+# .. tip::
 #
 #    You can find out more about AiiDA and its architecture in the `AiiDA documentation`_.
 
 # %%
-# (Optional) Start the AiiDA GUI
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# AiiDA GUI (Optional)
+# ~~~~~~~~~~~~~~~~~~~~
 #
 # Some features of ``WorkGraph`` are best demonstrated interactively.
 # For this, you can use the AiiDA GUI.
 # First, let's install the necessary packages:
 #
-# ::
+# .. code:: console
 #
-#    pip install aiida-gui aiida-gui-workgraph
+#    $ pip install aiida-gui aiida-gui-workgraph
 #
-# Once installed, open a terminal, and run:
+# Once installed, run the following to start the GUI:
 #
-# ::
+# .. code:: console
 #
-#    aiida-gui start
+#    $ aiida-gui start
 #
 # Go to http://localhost:8000/workgraph in your browser to view a table of executed ``WorkGraph`` processes.
 
@@ -90,10 +99,11 @@ load_profile()
 # Simple workflow
 # ---------------
 #
-# Suppose we want to calculate ``(x + y) * z`` in two steps:
+# Suppose you want to compute ``(x + y) * z``.
+# This requires two steps:
 #
-# - add ``x`` and ``y``
-# - then multiply the result with ``z``.
+# #. Adding ``x`` and ``y``, then
+# #. Multiplying the result with ``z``
 #
 # The simplest way to do this in ``WorkGraph`` is as follows:
 
@@ -105,20 +115,23 @@ with WorkGraph("AddMultiply") as wg:
 
 # %%
 # We've defined our first workflow using ``WorkGraph`` 🎉
-# It takes three arguments, ``x``, ``y``, and ``z``, adds the first two, multiplies the result by the third, and assigns the result as an output.
-# In the background, ``WorkGraph`` converts the addition and subsequent multiplication operations into tasks.
 #
-# ..tip::
+# The workflow expects three arguments, ``x``, ``y``, and ``z``.
+# It sets up as tasks the addition of the ``x`` and ``y`` (in parentheses), and the multiplication of the result by ``z``.
+# Finally, it assigns the final result as an output.
 #
-#   ``result`` is a special output port given by default.
+# .. tip::
+#
+#    ``result`` is a special output port given by default.
 #    However, you can also define your own output ports with ``wg.outputs.my_output = ...``.
-#    You can learn more about graph-level inputs and output in the :doc:`autogen/graph_level` section.
+#    You can learn more about graph-level inputs and output in the :doc:`../howto/autogen/graph_level` how-to section.
 #
 # We can visualize the workgraph to inspect the tasks and their connections.
 
 wg.to_html()
 
-# We can run it with
+# %%
+# Let's run it with some inputs:
 
 wg.run(inputs={"x": 2, "y": 3, "z": 4})
 
@@ -126,20 +139,23 @@ wg.run(inputs={"x": 2, "y": 3, "z": 4})
 # .. tip::
 #
 #    If you've configure a message broker in AiiDA, you can also submit the workgraph
-#    (queue it for execution) with ``wg.submit(inputs=...)``.
-#    You can learn more about this in the `AiiDA documentation_`.
-
-# %%
-# .. tip::
+#    (queue it for execution) with
 #
-#    If you're running the AiiDA GUI, you can visualize the executed workflow interactively.
-#    Click on the ``PK`` field of the submitted workflow (look for ``WorkGraph<AddMultiply>``) to view its details.
+#    .. code:: python
+#
+#       wg.submit(inputs=..., wait=True)
+#
+#    You can use ``wait=True`` to block the terminal or notebook until the workflow finishes.
+#    You can learn more about this in the `AiiDA documentation`_.
+
 
 # %%
+# Let's have a look at the result:
+
 print("Result:", wg.outputs.result.value)
 
 # %%
-# We can also access the results of the individual tasks.
+# We can also access the results of the individual tasks:
 
 print("Result of addition:", wg.tasks.op_add.outputs.result.value)
 print("Result of multiplication:", wg.tasks.op_mul.outputs.result.value)
@@ -147,15 +163,21 @@ print("Result of multiplication:", wg.tasks.op_mul.outputs.result.value)
 # %%
 # .. note::
 #
-#    Many Python operators are supported in `WorkGraph`` and are automatically converted to tasks with the name ``op_<operator>``, e.g. ``op_add``, ``op_mul``, ``op_lt``, ``op_eq``, and more.
+#    Many Python operators are supported in ``WorkGraph`` and are automatically converted to tasks with the name ``op_<operator>``, e.g. ``op_add``, ``op_mul``, ``op_lt``, ``op_eq``, and more.
+
+# %%
+# .. tip::
+#
+#    If you're running the AiiDA GUI, you can visualize the executed workflow interactively.
+#    Click on the ``PK`` field of the submitted workflow (look for ``WorkGraph<AddMultiply>``) to view its details.
 #
 # So far so good! We have a simple workflow, can visualize it, run/submit it with inputs, and inspect its outputs.
 # However, in practice, tasks will require operations more complex than simple arithmetics.
 # How can we handle these?
 
 # %%
-# ``Task``s
-# ---------
+# Defining a ``Task``
+# -------------------
 #
 # A ``Task`` is the basic building block of the ``WorkGraph``. It has inputs,
 # outputs, and an executor. Let's define the above operations as tasks.
@@ -174,7 +196,7 @@ def multiply(x, y):
 
 
 # %%
-# Let's see how we can use these tasks in a workgraph.
+# We can use these functional tasks directly in a workgraph as follows:
 
 with WorkGraph("AddMultiplyWithDefinedTasks") as wg:
     wg.inputs = dict.fromkeys(["x", "y", "z"])
@@ -185,31 +207,33 @@ with WorkGraph("AddMultiplyWithDefinedTasks") as wg:
 wg.to_html()
 
 # %%
-# Using ``add`` and ``multiply`` is quite similar to how functions are used in Python.
-# However, unlike the case of ``wg.inputs.x + wg.inputs.y``, calling a ``Task`` returns an outputs namespace.
+# We use ``add`` and ``multiply`` here quite similarly to how functions are used in Python.
+# The main difference is that we do not pass inputs by value, but rather by reference to the workgraph inputs, or sockets.
+# This creates a link, or dependency, between input/output sockets.
+#
+# Moreover, unlike the case of ``wg.inputs.x + wg.inputs.y``, calling a ``Task`` returns an outputs (sockets) namespace.
 # This is because in general, a task can have multiple outputs.
-# For example, let's compare calling ``add``
+# For example, let's compare calling ``add``:
 
 add(x=2, y=3)
 
 # %%
-# and calling the following:
+# with calling the following:
 
 
 @task(outputs=["count", "even_numbers"])
 def count_even_numbers(numbers):
     even_numbers = [n for n in numbers if n % 2 == 0]
-    return {
-        "count": len(even_numbers),
-        "even_numbers": even_numbers,
-    }
+    return len(even_numbers), even_numbers
 
 
 count_even_numbers(numbers=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 # %%
-# Without explicitly specifying the expected outputs of a task, we get a default ``result`` socket (available output).
-# When we do specify our outputs, as done here, they become available at ``<task_name>.outputs.<output_name>`` post-execution.
+# We can see that without explicitly specifying the expected outputs of a task, we get a default ``result`` socket.
+# When we do specify our outputs, they become available at ``<task_name>.outputs.<output_name>`` post-execution.
+#
+# Let's use this task in a workgraph to count the even numbers in a list.
 
 with WorkGraph("CountEvenNumbers") as wg:
     wg.inputs = dict.fromkeys(["numbers"])
@@ -221,6 +245,9 @@ with WorkGraph("CountEvenNumbers") as wg:
 
 wg.to_html()
 
+# %%
+# Finally, let's submit the workflow and inspect its results:
+
 wg.submit(
     inputs={
         "numbers": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
@@ -228,7 +255,7 @@ wg.submit(
     wait=True,
 )
 
-print("Result:")
+print("\nResult:")
 print("  Count:", wg.outputs.count.value)
 print("  Even numbers:", wg.outputs.even_numbers.value)
 
@@ -267,7 +294,7 @@ def SumEvenNumbers(numbers):
     return sum_numbers(numbers=even_numbers_output.even_numbers).result
 
 
-with WorkGraph() as wg:
+with WorkGraph("SumEvenRandomNumbers") as wg:
     wg.inputs = dict.fromkeys(["minimum", "maximum"])
     n = random_number_generator(wg.inputs.minimum, wg.inputs.maximum).result
     numbers = generate_numbers(n=n).result
@@ -276,27 +303,29 @@ with WorkGraph() as wg:
 wg.to_html()
 
 # %%
-# Here we see ``SumEvenNumbers`` as a black-box task.
-# To inspect its internal tasks, we can use the ``get_graph`` method as follows:
-
-SumEvenNumbers.get_graph(
-    numbers=[*range(42)]
-).to_html()  # the input doesn't matter here
-
-# %%
-# .. note::
-#
-#    When using the AiiDA GUI to inspect executed workflows, sub-workflows can be expanded interactively to inspect their internal tasks and connections.
-
-# %%
 # .. note::
 #
 #    Though not strictly required, we name our graph tasks using camel case to distinguish them from regular tasks.
 #    If not explicitly overridden, the name of the decorated function will be used as the name of the task when inspecting processes using, for example, ``verdi process list``.
+#
+# Here we see ``SumEvenNumbers`` as a black-box task.
+# To inspect its internal tasks, we can use the ``get_graph`` method as follows:
+
+SumEvenNumbers.get_graph(numbers=[*range(42)]).to_html()
 
 # %%
-# Complex workflows
-# -----------------
+# .. tip::
+#
+#    When using the AiiDA GUI to inspect executed workflows, sub-workflows can be expanded interactively to inspect their internal tasks and connections.
+
+# %%
+# .. tip::
+#
+#    To learn more about workflow composition, see the :doc:`../howto/autogen/combine_workgraphs` how-to section.
+
+# %%
+# Complex workflow
+# ----------------
 #
 # So far, our workflows have been simple and linear.
 # However, often we need to create more complex workflows that involve branching, loops, and other control structures.
@@ -304,9 +333,9 @@ SumEvenNumbers.get_graph(
 # Suppose we want to conditionally sum a set of numbers based on some random input.
 # If the input is greather than 50, we filter out the even numbers and sum them.
 # Otherwise, we just sum all the numbers.
-# Let's try to define this workflow using what we've learned above.
-
-# .. code-block:: python
+# Let's try to define this workflow using what we've learned so far:
+#
+# .. code:: python
 #
 #    with WorkGraph("ComplexWorkflow") as wg:
 #        wg.inputs = dict.fromkeys(["minimum", "maximum"])
@@ -319,12 +348,15 @@ SumEvenNumbers.get_graph(
 #        wg.outputs.sum = the_sum
 
 # %%
-# This seems straightforward and Pythonic. However, if won't work as expected.
+# This seems straightforward.
+# However, **it won't work as expected**.
 # The reason is that the ``if`` statement requires the value of ``n``, which is not available at the time of the workflow definition.
+#
 # To handle this conditional logic, and any other flow control for that matter, we must queue it as a graph task.
 # Another way to think of it is that any Pythonic control flow statement must be deferred to runtime.
-# We do this via task encapsulation.
-# Let's do this for the above example.
+# For this, **we need task encapsulation**.
+#
+# Let's do this now for the above example:
 
 
 @task.graph
@@ -345,17 +377,20 @@ wg.to_html()
 # %%
 #
 # By treating the conditional logic as a task, there is no need to know the value of ``n`` at workflow definition time, as the conditional statement will only evaluate when the workflow is executed.
-# This allows us to define dynamic workflows that decide their flow at runtime.
-# To see this more clearly, we can inspect the graph of the ``ConditionalSum`` task given inputs on both ends of the condition.
+# This allows us to define dynamic workflows that **decide their flow at runtime**.
+#
+# To see this more clearly, we can inspect the graph of the ``ConditionalSum`` task given inputs on both ends of the condition:
 
-ConditionalSum.get_graph(numbers=[*range(40)]).to_html()
-
-# %%
-ConditionalSum.get_graph(numbers=[*range(60)]).to_html()
+ConditionalSum.get_graph(numbers=[*range(40)]).to_html("html/ConditionalSumTrue.html")
 
 # %%
-# You can learn more about ``WorkGraph`` flow control in the :doc:`autogen/control-flow` section.
-# You can also learn more about the ``@task.graph`` decorator in the :doc:`autogen/graph_builder_concept` section.
+ConditionalSum.get_graph(numbers=[*range(60)]).to_html("html/ConditionalSumFalse.html")
+
+# %%
+# You can learn more about ``WorkGraph`` flow control in the :doc:`../howto/autogen/control-flow` how-to section.
+# You can also learn more about the ``@task.graph`` decorator in the :doc:`../concept/autogen/graph_builder_concept` concept section.
+
+set_aiida_loglevel("ERROR")
 
 # %%
 # What’s Next
@@ -374,7 +409,7 @@ ConditionalSum.get_graph(numbers=[*range(60)]).to_html()
 # |                                                         | ``if``, ``for``, ``while`` and ``context``.            |
 # |                                                         |                                                        |
 # +---------------------------------------------------------+--------------------------------------------------------+
-# | `Migration <migration_from_aiida_core/index.rst>`       | Migration guide for users interested in transferring   |
+# | `Migration <migration_from_aiida_core/index.rst>`__     | Migration guide for users interested in transferring   |
 # |                                                         | aiida-core processes (e.g. ``WorkChain``, ``CalcJob``) |
 # |                                                         | to ``WorkGraph``.                                      |
 # |                                                         |                                                        |
