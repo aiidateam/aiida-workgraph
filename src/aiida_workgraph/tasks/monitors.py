5c10a5e704c1e967f2a0918c760ed084661c5d2c
Edan Bainglass <edan.bainglass@gmail.com>
2025-08-05 16:29:17 +0200

    Discard class-based monitor tasks and point entrypoints to functional monitor tasks

--- src/aiida_workgraph/tasks/monitors.py
+++ src/aiida_workgraph/tasks/monitors.py
@@ -2,7 +2,6 @@ import datetime
 import typing as t
 
 from aiida_workgraph import task
-from aiida_workgraph.tasks.factory.awaitable_task import MonitorFunctionTask
 
 
 async def monitor(function, interval, timeout, *args, **kwargs):
@@ -73,99 +72,4 @@ def monitor_task(task_name: str, workgraph_pk: int = None, workgraph_name: str =
     node = builder.first()[0]
     state = node.task_states.get(task_name, "")
     print(f"Task state: {state}")
-    if state in ["FINISHED", "FAILED", "SKIPPED"]:
-        return True
-    else:
-        return False
-
-
-class TimeMonitor(MonitorFunctionTask):
-    """Monitor the time"""
-
-    identifier = "workgraph.time_monitor"
-    name = "TimeMonitor"
-    catalog = "Monitor"
-
-    def create_sockets(self) -> None:
-        self.inputs._clear()
-        self.outputs._clear()
-        self.add_input("workgraph.any", "time")
-        inp = self.add_input("workgraph.any", "interval")
-        inp.add_property("workgraph.any", default=1.0)
-        inp = self.add_input("workgraph.any", "timeout")
-        inp.add_property("workgraph.any", default=86400.0)
-        self.add_input(
-            "workgraph.any", "_wait", metadata={"arg_type": "none"}, link_limit=100000
-        )
-        inp._link_limit = 100000
-        self.add_output("workgraph.any", "result")
-        self.add_output("workgraph.any", "_wait")
-
-    def get_executor(self):
-        executor = {
-            "module_path": "aiida_workgraph.tasks.monitors",
-            "callable_name": "monitor_time",
-        }
-        return executor
-
-
-class FileMonitor(MonitorFunctionTask):
-    """Monitor the file"""
-
-    identifier = "workgraph.file_monitor"
-    name = "FileMonitor"
-    catalog = "Monitor"
-
-    def create_sockets(self) -> None:
-        self.inputs._clear()
-        self.outputs._clear()
-        self.add_input("workgraph.any", "filepath")
-        inp = self.add_input("workgraph.any", "interval")
-        inp.add_property("workgraph.any", default=1.0)
-        inp = self.add_input("workgraph.any", "timeout")
-        inp.add_property("workgraph.any", default=86400.0)
-        self.add_input(
-            "workgraph.any", "_wait", metadata={"arg_type": "none"}, link_limit=100000
-        )
-        inp._link_limit = 100000
-        self.add_output("workgraph.any", "result")
-        self.add_output("workgraph.any", "_wait")
-
-    def get_executor(self):
-        executor = {
-            "module_path": "aiida_workgraph.tasks.monitors",
-            "callable_name": "monitor_file",
-        }
-        return executor
-
-
-class TaskMonitor(MonitorFunctionTask):
-    """Monitor the file"""
-
-    identifier = "workgraph.task_monitor"
-    name = "TaskMonitor"
-    catalog = "Monitor"
-
-    def create_sockets(self) -> None:
-        self.inputs._clear()
-        self.outputs._clear()
-        self.add_input("workgraph.any", "workgraph_pk")
-        self.add_input("workgraph.any", "workgraph_name")
-        self.add_input("workgraph.any", "task_name")
-        inp = self.add_input("workgraph.any", "interval")
-        inp.add_property("workgraph.any", default=1.0)
-        inp = self.add_input("workgraph.any", "timeout")
-        inp.add_property("workgraph.any", default=86400.0)
-        self.add_input(
-            "workgraph.any", "_wait", metadata={"arg_type": "none"}, link_limit=100000
-        )
-        inp._link_limit = 100000
-        self.add_output("workgraph.any", "result")
-        self.add_output("workgraph.any", "_wait")
-
-    def get_executor(self):
-        executor = {
-            "module_path": "aiida_workgraph.tasks.monitors",
-            "callable_name": "monitor_task",
-        }
-        return executor
+    return state in ["FINISHED", "FAILED", "SKIPPED"]
