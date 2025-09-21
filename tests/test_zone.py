1404059d19675719c7d8aa3ab1938e5bf0436dee
superstar54 <xingwang1991@gmail.com>
2025-09-21 21:23:59 +0200

    Add test for running zone

--- tests/test_zone.py
+++ tests/test_zone.py
@@ -8,12 +8,13 @@ def test_zone_task(decorated_add):
     add1 = wg.add_task(decorated_add, name='add1', x=1, y=1)
     zone1 = wg.add_task('workgraph.zone', name='zone1')
     zone1.add_task(decorated_add, name='add2', x=1, y=1)
-    zone1.add_task(decorated_add, name='add3', x=1, y=add1.outputs.result)
     wg.add_task(decorated_add, name='add4', x=1, y=wg.tasks.add2.outputs.result)
-    wg.add_task(decorated_add, name='add5', x=1, y=wg.tasks.add3.outputs.result)
+    zone1.add_task(decorated_add, name='add3', x=1, y=add1.outputs.result)
     connectivity = wg.build_connectivity()
     assert connectivity['zone']['add4']['input_tasks'] == ['zone1']
-    assert connectivity['zone']['add5']['input_tasks'] == ['zone1']
+    wg.run()
+    # add4 should wait the whole zone1 to finish, thus its mtime should be larger than add3
+    assert wg.tasks.add4.node.mtime > wg.tasks.add3.node.mtime
 
 
 def test_zone_context_manager(decorated_add):
