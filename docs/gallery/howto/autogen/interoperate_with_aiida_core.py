ace781e1c61524576a0119ddd30489eee16eb9ce
superstar54 <xingwang1991@gmail.com>
2025-10-23 15:01:30 +0200

    Update the docs for calcfunction and workfunction

--- docs/gallery/howto/autogen/interoperate_with_aiida_core.py
+++ docs/gallery/howto/autogen/interoperate_with_aiida_core.py
@@ -19,12 +19,8 @@ Interoperate with ``aiida-core`` components
 #    If you’re unfamiliar with them, please refer to the official documentation on `Calculations <https://aiida.readthedocs.io/projects/aiida-core/en/stable/topics/calculations/index.html>`_ and `Workflows <https://aiida.readthedocs.io/projects/aiida-core/en/stable/topics/workflows/index.html>`_.
 
 import typing as t
-
 from aiida import load_profile, orm
-from aiida.calculations.arithmetic.add import ArithmeticAddCalculation
-from aiida.engine import calcfunction, workfunction
-from aiida.workflows.arithmetic.multiply_add import MultiplyAddWorkChain
-
+from aiida.engine import calcfunction
 from aiida_workgraph import namespace, task
 
 load_profile()
@@ -38,21 +34,37 @@ load_profile()
 #
 # In the following example, we combine all four ``aiida-core`` processes in a single ``WorkGraph``, connecting their inputs and outputs as needed.
 #
+# Calcfunction and workfunction
+# ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 # Let's first define a ``calcfunction`` and ``workfunction``
+from aiida import orm
 
 
 @calcfunction
-def add(x, y):
-    return x + y
-
-
-@workfunction
-def add_more(x, y, z):
-    sum_x_y = add(x, y)
-    return add(sum_x_y, z)
+def add_multiply(x, y):
+    return {'sum': orm.Int(x + y), 'product': orm.Int(x * y)}
 
 
 # %%
+# To use ``aiida-core`` components in a ``WorkGraph``, we can simply cast them as tasks using the ``task`` decorator functionally (``task(<aiida-core-component>)``).
+
+add_multiply_task = task(outputs=namespace(sum=int, product=int))(add_multiply)
+
+# %%
+# One can also use the decorator syntax directly
+
+
+@task.calcfunction(outputs=namespace(sum=int, product=int))
+def add_multiply_task(x, y):
+    return {'sum': orm.Int(x + y), 'product': orm.Int(x * y)}
+
+
+# %%
+# The same syntax applies to `workfunction`.
+
+
+# %%
+#
 # .. important::
 #
 #    The ``task`` decorator accepts the same arguments when used functionally.
@@ -62,34 +74,62 @@ def add_more(x, y, z):
 #      - When the AiiDA process provides a single output (e.g., ``return x``), the socket name defaults to ``result``. You may override it (e.g., ``task(outputs=["my_sum"])(add)(...)``) for clearer graph labels, understanding the provenance graph will still record ``result``.
 #      - When the process returns a dictionary (e.g., ``return {"x": x, "y": y}``), you must assign output socket names explicitly if you want to reference individual entries. See :doc:`../../gallery/howto/autogen/annotate_inputs_outputs` for more on namespaces and dynamic sockets.
 #
+# CalcJob and WorkChain
+# ^^^^^^^^^^^^^^^^^^^^^^^
+# Similarly, ``CalcJob`` and ``WorkChain`` can also be cast as tasks using the ``task`` decorator.
+
+from aiida.workflows.arithmetic.multiply_add import MultiplyAddWorkChain
+from aiida.calculations.arithmetic.add import ArithmeticAddCalculation
+
+
+ArithmeticAddTask = task(ArithmeticAddCalculation)
+MultiplyAddTask = task(MultiplyAddWorkChain)
 
 
 # %%
-# To use ``aiida-core`` components in a ``WorkGraph``, we can simply cast them as tasks using the ``task`` decorator functionally (``task(<aiida-core-component>)``).
-# Task functional components can then be called functionally with their respective inputs, linking outputs to inputs as needed.
+# .. important::
+#
+#    For ``WorkChain`` and ``CalcJob``, inputs and outputs are determined by the AiiDA process definition and cannot be overridden by passing the `inputs` or `outputs` arguments to the ``task`` decorator.
+#
+
+# %%
+# Task components can then be called functionally with their respective inputs, linking outputs to inputs as needed.
+
+
+def add(x, y):
+    return x + y
+
+
+def add_more(x, y, z):
+    sum_x_y = add(x, y)
+    return add(sum_x_y, z)
+
+
+AddTask = task(add)
+AddMoreTask = task(add_more)
 
 
 @task.graph
 def AiiDAComponentsWorkflow():
-    calcjob_sum = task(ArithmeticAddCalculation)(
+    calcjob_sum = ArithmeticAddTask(
         code=orm.load_code('add@localhost'),
         x=1,
         y=2,
     ).sum  # `ArithmeticAddCalculation` explicitly defines a 'sum' output
 
-    calcfunction_sum = task(add)(
+    calcfunction_sum = AddTask(
         x=calcjob_sum,
         y=4,
     ).result  # `calcfunction` implicitly defines a 'result' output
 
-    workchain_result = task(MultiplyAddWorkChain)(
+    workchain_result = MultiplyAddTask(
         code=orm.load_code('add@localhost'),
         x=calcfunction_sum,
         y=2,
         z=3,
     ).result  # `MultiplyAddWorkChain` explicitly defines a 'result' output
 
-    workfunction_sum = task(add_more)(
+    workfunction_sum = AddMoreTask(
         x=workchain_result,
         y=2,
         z=3,
@@ -100,12 +140,6 @@ def AiiDAComponentsWorkflow():
 
 wg = AiiDAComponentsWorkflow.build()
 wg.to_html()
-
-# %%
-# .. important::
-#
-#    For ``WorkChain`` and ``CalcJob``, inputs and outputs are determined by the AiiDA process definition and cannot be overridden by passing the `inputs` or `outputs` arguments to the ``task`` decorator.
-
 
 # %%
 # Let's run our ``aiida-core``-powered ``WorkGraph`` and examine the provenance graph:
